e10a8ad479772a3e0507709210168ceadbe767fc
Clems-Laptop <steaksaignant7@gmail.com>
2019-01-11 11:20:00 +0100

    Classes push

---
+++ app/src/main/java/com/example/thoma/tpnot_11_01_2019/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.thoma.tpnot_11_01_2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
